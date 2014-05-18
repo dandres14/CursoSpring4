dc0486ede012766451c1c3cb5e35b09122a0691f
Andrés <Andrs@Andres-PC>
2014-05-18 01:31:08 -0500

    Cuentas

--- cuentas/src/com/mycompany/cuentas/controller/CuentaController.java
+++ cuentas/src/com/mycompany/cuentas/controller/CuentaController.java
@@ -9,31 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.mycompany.cuentas.dao.CuentaDAO;
 import com.mycompany.cuentas.modelo.Cuenta;
 
+// Controller Cuenta
 @Controller
 public class CuentaController {
 	@RequestMapping("/form")
-	public String mainFormulario(){
+	public String mainFormulario() {
 		return "cuenta/formulario";
 	}
-	
+
 	@RequestMapping("/agregarCuenta")
-	public String agregarCuenta(Cuenta cuenta){
-		System.out.println("La cuenta agregada es "+cuenta.getDescripcion());
+	public String agregarCuenta(Cuenta cuenta) {
+		System.out.println("La cuenta agregada es " + cuenta.getDescripcion());
 		CuentaDAO dao = new CuentaDAO();
 		dao.agregar(cuenta);
 		return "cuenta/cuenta-agregada";
 	}
-	
-//	@RequestMapping("/listarCuentas")
-//	public ModelAndView listarCuentas(){
-//		CuentaDAO dao = new CuentaDAO();
-//		List<Cuenta> cuentas = dao.listar();
-//		
-//		ModelAndView mv = new ModelAndView("cuenta/listar-cuenta");
-//		mv.addObject("cuentas", cuentas);
-//		return mv;
-//	}
-	
+
+	// @RequestMapping("/listarCuentas")
+	// public ModelAndView listarCuentas(){
+	// CuentaDAO dao = new CuentaDAO();
+	// List<Cuenta> cuentas = dao.listar();
+	//
+	// ModelAndView mv = new ModelAndView("cuenta/listar-cuenta");
+	// mv.addObject("cuentas", cuentas);
+	// return mv;
+	// }
+
 	@RequestMapping("/listarCuentas")
 	public String listar(Model mv) {
 		CuentaDAO dao = new CuentaDAO();
@@ -41,21 +42,21 @@ public class CuentaController {
 		mv.addAttribute("cuentas", cuentas);
 		return "cuenta/listar-cuenta";
 	}
-	
+
 	@RequestMapping("/eliminarCuenta")
 	public String remove(Cuenta cuenta) {
 		CuentaDAO dao = new CuentaDAO();
 		dao.eliminar(cuenta);
 		return "redirect:listarCuentas";
 	}
-	
+
 	@RequestMapping("/muestraCuenta")
 	public String muestra(Long id, Model model) {
 		CuentaDAO dao = new CuentaDAO();
 		model.addAttribute("cuenta", dao.buscarPorId(id));
 		return "cuenta/muestra";
 	}
-	
+
 	@RequestMapping("/modificarCuenta")
 	public String modificar(Cuenta cuenta) {
 		CuentaDAO dao = new CuentaDAO();
